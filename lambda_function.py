df32de8d1e2ebd902d734dbdfebbcc94a9318b72
hemjeet <hemjeetsingh88@gmail.com>
2025-04-20 22:12:19 +0530

    3rd

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,8 @@ import pandas as pd
 import pickle
 import seaborn as sns
 
+# def handler(event, context):
+#     return 'Hello from AWS Lambda using Python and hemjeet (Auto update lambda(CI/CD (code deploy)))' + sys.version + '!'
+
 def handler(event, context):
-    return 'Hello from AWS Lambda using Python and hemjeet (Auto update lambda(CI/CD (code deploy)))' + sys.version + '!'
+    return 'Hello from AWS Lambda. Code updated in dev alias' + sys.version + '!'
