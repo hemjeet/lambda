14d0a80b03882eb5d4ae290b785d7c2512990207
hemjeet <hemjeetsingh88@gmail.com>
2025-04-20 19:18:52 +0530

    56

--- lambda_function.py
+++ lambda_function.py
@@ -4,4 +4,4 @@ import pickle
 import seaborn as sns
 
 def handler(event, context):
-    return 'Hello from AWS Lambda using Python and hemjeet (Auto update lambda(CI/CD))' + sys.version + '!'
+    return 'Hello from AWS Lambda using Python and hemjeet (Auto update lambda(CI/CD (code deploy)))' + sys.version + '!'
